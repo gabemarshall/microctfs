0cc27fe77d6b17145ed75755e63642724b0ee16e
Gabe Marshall <gabemarshall@me.com>
2016-01-15 08:08:37 -0600

    Replaced zip file with folder containing the challenge

---
+++ logviewer/300/index.php
@@ -0,0 +1,81 @@
+<?php
+// Check if a weird user agent is being used
+if (strpos($_SERVER['HTTP_USER_AGENT'],'Mozilla') !== false) {
+header("X-Nothing-2See: here");
+} else { echo "<script>alert('I don\'t like what you\'re doing!')</script>";die();}
+
+// Custom Headers
+header("X-XSS-Protection: 0");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css" media="screen" />
+<link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel=
+    'stylesheet' type='text/css'>
+<link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" />
+
+<!--<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
+<script src="../js/html5.js"></script>
+<script src="../js/jquery.min.js"></script>
+
+<title>Sys Admin</title>
+
+</head>
+
+<body>
+
+<div id="main" style="padding-left:30px;padding-top:30px;">
+
+    <h1>Sys Admin - Log Viewer</h1>
+    <br>
+<p>File viewing options</p>
+<FORM method="post" action="index.php">
+<b>Drop-Down Menu (Single File Selection) </b><br>
+File Name
+<select name="log">
+  <option value="log01">log01</option>
+  <option value="log02">log02</option>
+  <option value="log03">log03</option>
+  <option value="log04">log04</option>
+</select>
+<input type="submit" value="Submit">
+</FORM>
+</div>
+<?php
+    if(isset($_POST["log"]))
+    {
+      $log = $_POST["log"];
+    } 
+    if(strlen($log)<25 && strpos($log, 'ping') == false)
+    {
+       echo(shell_exec("cat `pwd`/" . $_POST["log"] . ".log"));
+    }
+    else {
+       echo "Error, request failed validation checks.";
+       echo $_POST["log"];
+    }
+?>
+<script>
+var debug = document.getElementById("subnav")
+
+function _Debug_(p) {
+    p = p.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var r = new RegExp("[\\?&]" + p + "=([^&#]*)"),
+    results = r.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+if(location.search){
+    var val = _Debug_("param");
+    debug.innerHTML += val;
+    console.log(1);
+}
+</script>
+</body>
+
+</html>
