551dd9b9620e80859ba3e87c591e1de901c844cf
Gabe Marshall <gabemarshall@me.com>
2016-02-19 11:01:40 -0500

    Added convenience scripts to start, stop, restart containers

---
+++ restart.sh
@@ -0,0 +1,4 @@
+docker rm -f sqli_chal && docker run -d -p 8888:80 --name sqli_chal sqli
+docker rm -f log_challenge && docker run -d -p 8000:80 --name log_challenge logviewer
+docker rm -f tc_chal && docker run -d -p 8080:8080 --name tc_chal tcmanager
+docker rm -f geddy_chal && docker run -d -p 4000:4000 --name geddy_chal geddy
---
+++ stop.sh
@@ -0,0 +1,4 @@
+docker rm -f log_challenge
+docker rm -f sqli_chal
+docker rm -f tc_chal
+docker rm -f geddy_chal
